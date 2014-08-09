0d79d3ba380388d523cd52cdfefd3b13040d085d
csiwale <chisanga.siwale@gmail.com>
2014-08-09 23:53:35 +0200

    Changed exception handling to conform to python 2.x

--- TweetStream.py
+++ TweetStream.py
@@ -14,7 +14,7 @@ auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
 twitter_api = twitter.Twitter(auth=auth)
 
 q = 'Zambia' #Comma-separated list of terms
-locations = "-18.141344,22.070259,-7.951179,33.485053"
+locations = "22.070259,-18.141344,33.485053,-7.951179"
 
 print ('Filtering the public timeline for track="%s"' % (q,), file=sys.stderr)
 
@@ -23,14 +23,22 @@ twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
 
 #See https://dev.twitter.com/docs/streaming-apis
 #stream = twitter_stream.statuses.filter(track=q)
-#stream = twitter_stream.statuses.filter(locations=locations)
-stream = twitter_stream.statuses.sample()
+stream = twitter_stream.statuses.filter(locations=locations)
+#stream = twitter_stream.statuses.sample()
+
+def in_zambia(coord):
+    try:
+        if not (coord[0] >= 22.070259 & coord[0] <= 33.485053) & (coord[1] >= -18.141344 & coord[1] <= -7.951179):
+            return False
+    except BaseException:
+        return False
+    return True
 
 for tweet in stream:
     try:
         if "zambia" in tweet['user']['location'].lower():
-            print(tweet.strip())
-    except KeyError as e:
+            print(tweet['text'])
+    except KeyError:
         continue
 # Save to a database in a particular collection
 
