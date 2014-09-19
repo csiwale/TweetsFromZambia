6338193e8302b62968b326b61b30bf32f59bf785
csiwale <chisanga.siwale@gmail.com>
2014-09-19 17:59:01 +0200

    Added exception handling for http requests

--- TweetStream.py
+++ TweetStream.py
@@ -2,29 +2,22 @@ import twitter
 import json
 import sys
 import oauth2
+import time
+from urllib2 import URLError
+from httplib import BadStatusLine
 
-CONSUMER_KEY = 'uz1mF8q2Ycq8hTtiq1pGukQbl'
-CONSUMER_SECRET = 'kaPNxbCI0p31Xu3t2ASw80FuTnjVr9CBDB3ZnZScojxM7epKjP'
-OAUTH_TOKEN = '49647130-HnVRebqlYGCUIGFVEVuw3ngJeuQD4wCoLG3SIoMbF'
-OAUTH_TOKEN_SECRET = 'TBW3iQnNQqEBLyTvhDdvIvAqISn9MSAE8XsdnB2U39m8R'
+def oauth_login():
+    CONSUMER_KEY = 'uz1mF8q2Ycq8hTtiq1pGukQbl'
+    CONSUMER_SECRET = 'kaPNxbCI0p31Xu3t2ASw80FuTnjVr9CBDB3ZnZScojxM7epKjP'
+    OAUTH_TOKEN = '49647130-HnVRebqlYGCUIGFVEVuw3ngJeuQD4wCoLG3SIoMbF'
+    OAUTH_TOKEN_SECRET = 'TBW3iQnNQqEBLyTvhDdvIvAqISn9MSAE8XsdnB2U39m8R'
 
-auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
-                           CONSUMER_KEY, CONSUMER_SECRET)
+    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,CONSUMER_KEY, CONSUMER_SECRET)
 
-twitter_api = twitter.Twitter(auth=auth)
+    twitter_api = twitter.Twitter(auth=auth)
 
-q = 'Zambia' #Comma-separated list of terms
-locations = "22.070259,-18.141344,33.485053,-7.951179"
+    return twitter_api
 
-print ('Filtering the public timeline for track="%s"' % (q,), file=sys.stderr)
-
-#Reference the self.auth parameter
-twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
-
-#See https://dev.twitter.com/docs/streaming-apis
-#stream = twitter_stream.statuses.filter(track=q)
-stream = twitter_stream.statuses.filter(locations=locations)
-#stream = twitter_stream.statuses.sample()
 
 def in_zambia(coord):
     try:
@@ -34,12 +27,90 @@ def in_zambia(coord):
         return False
     return True
 
-for tweet in stream:
-    try:
-        if "zambia" in tweet['user']['location'].lower():
-            print(tweet.strip())
-    except KeyError:
-        continue
+def make_twitter_request(twitter_api_func, max_errors=10, *args, **kw):
+    # A nested helper function that handles common HTTPErrors. Return an updated
+    # value for wait_period if the problem is a 500 level error. Block until the
+    # rate limit is reset if it's a rate limiting issue (429 error). Returns None
+    # for 401 and 404 errors, which requires special handling by the caller.
+    def handle_twitter_http_error(e, wait_period=2, sleep_when_rate_limited=True):
+        if wait_period > 3600: # Seconds
+            print >> sys.stderr, 'Too many retries. Quitting.'
+            raise e
+        # See https://dev.twitter.com/docs/error-codes-responses for common codes
+        if e.e.code == 401:
+            print >> sys.stderr, 'Encountered 401 Error (Not Authorized)'
+            return None
+        elif e.e.code == 404:
+            print >> sys.stderr, 'Encountered 404 Error (Not Found)'
+            return None
+        elif e.e.code == 429:
+            print >> sys.stderr, 'Encountered 429 Error (Rate Limit Exceeded)'
+            if sleep_when_rate_limited:
+                print >> sys.stderr, "Retrying in 15 minutes...ZzZ..."
+                sys.stderr.flush()
+                time.sleep(60*15 + 5)
+                print >> sys.stderr, '...ZzZ...Awake now and trying again.'
+                return 2
+            else:
+                raise e # Caller must handle the rate limiting issue
+        elif e.e.code in (500, 502, 503, 504):
+            print >> sys.stderr, 'Encountered %i Error. Retrying in %i seconds' % \
+                (e.e.code, wait_period)
+            time.sleep(wait_period)
+            wait_period *= 1.5
+            return wait_period
+        else:
+            raise e
+    # End of nested helper function
+
+    wait_period = 2
+    error_count = 0
+    while True:
+        try:
+            return twitter_api_func(*args, **kw)
+        except twitter.api.TwitterHTTPError, e:
+            error_count = 0
+            wait_period = handle_twitter_http_error(e, wait_period)
+            if wait_period is None:
+                return
+        except URLError, e:
+            error_count += 1
+            print >> sys.stderr, "URLError encountered. Continuing."
+            if error_count > max_errors:
+                print >> sys.stderr, "Too many consecutive errors...bailing out."
+                raise
+        except BadStatusLine, e:
+            error_count += 1
+            print >> sys.stderr, "BadStatusLine encountered. Continuing."
+            if error_count > max_errors:
+                print >> sys.stderr, "Too many consecutive errors...bailing out."
+                raise
+
+
 # Save to a database in a particular collection
+def saveTweets(fileName, data):
+    with open('data.txt', 'a') as outfile:
+        json.dump(data, outfile)
 
+if __name__ == '__main__':
+    twitter_api = oauth_login()
 
+    locations = "22.070259,-18.141344,33.485053,-7.951179"
+    q = 'Zambia '+locations #Comma-separated list of terms
+
+    print >> sys.stderr, 'Filtering the public timeline for track = "%s"' % (q,)
+
+    #Reference the self.auth parameter
+    twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
+
+    #See https://dev.twitter.com/docs/streaming-apis
+    #stream = twitter_stream.statuses.filter(track=q)
+    #stream = twitter_stream.statuses.filter(locations=locations)
+    stream = make_twitter_request(twitter_stream.statuses.filter, locations=locations)
+    #stream = twitter_stream.statuses.sample()
+    for tweet in stream:
+        try:
+            if "zambia" in tweet['user']['location'].lower():
+                print(tweet)
+        except KeyError:
+            continue
