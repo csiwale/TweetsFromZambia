f71bfc2461fca52363ab56cec2e7aa191bd79ef2
csiwale <chisanga.siwale@gmail.com>
2014-10-22 16:41:41 +0200

    Stop_words

--- FrequencyDistribution.py
+++ FrequencyDistribution.py
@@ -4,11 +4,25 @@ import sys
 from collections import Counter
 from prettytable import PrettyTable
 
-def get_words(file):
-    with open(file, 'r') as f:
-        words = f.readlines()
-        c = Counter(words)
-        print c.most_common()[:10]
 
 if __name__ == '__main__':
-    get_words(sys.argv[1])
+    words = open(sys.argv[1])
+    hashtags = open(sys.argv[2])
+    tweet_texts = open(sys.argv[3])
+    screen_names = open((sys.argv[4]))
+    source = open((sys.argv[5]))
+
+    words = words.readlines()
+    hashtags = hashtags.readlines()
+    tweet_texts = tweet_texts.readlines()
+    screen_names = screen_names.readlines()
+    source = source.readlines()
+
+    #c = Counter(words)
+    #print (c.most_common()[:10])
+    for label, data in (('Word', words), ('Word', words), ('Hashtag', hashtags), ('Tweet Text', tweet_texts), ('Screen Name', screen_names), ('Source', source)):
+        pt = PrettyTable(field_names=[label, 'Count'])
+        c = Counter(data)
+        [pt.add_row(kv) for kv in c.most_common()[:10]]
+        pt.align[label], pt.align['Count'] = 'l', 'r' # Set column alignment
+        print (pt)
--- GetEntities.py
+++ GetEntities.py
@@ -11,43 +11,55 @@ def load_json(filename):
 
 def save_words(tweets):
     for line in tweets:
-        line = line.decode('ascii')
+        #line = line.decode('ascii')
         tweet = json.loads(line)
         with open('words.txt', 'a') as words:
             try:
                 for w in tweet['text'].split():
-                    w = w.encode('ascii')
+                    #w = w.encode('ascii')
                     words.write(w+'\n')
             except Exception as e:
                 print (e)
 
 def save_texts(tweets):
     for line in tweets:
-        line = line.decode('ascii')
+        #line = line.decode('ascii')
         tweet = json.loads(line)
         with open('tweet_texts.txt', 'a') as tweet_texts:
             try:
-                tweet_texts.write(tweet['text'], '\n')
+                tweet_texts.write(tweet['text'] + '\n')
             except Exception as e:
                 print (e)
 
 def save_screen_names(tweets):
     for line in tweets:
-        line = line.decode('ascii')
+        #line = line.decode('ascii')
         tweet = json.loads(line)
         with open('screen_names.txt', 'a') as screen_names:
             try:
-                screen_names.write(tweet['entities']['user_mentions'][0]['screen_name'], '\n')
+                screen_names.write(tweet['entities']['user_mentions'][0]['screen_name'] + '\n')
+            except Exception as e:
+                print (e)
+
+def save_source(tweets):
+    for line in tweets:
+        #line = line.decode('ascii')
+        tweet = json.loads(line)
+        with open('source.txt', 'a') as source:
+            try:
+                matchobj = re.match('<a (.*)>(.*)</a>', tweet['source'])
+                print(matchobj.group(2))
+                source.write(matchobj.group(2) + '\n')
             except Exception as e:
                 print (e)
 
 def save_hastags(tweets):
     for line in tweets:
-        line = line.decode('ascii')
+        #line = line.decode('ascii')
         tweet = json.loads(line)
         with open('hashtags.txt', 'a') as hashtags:
             try:
-                hashtags.write(tweet['entities']['hashtags'][0]['text'], '\n')
+                hashtags.write(tweet['entities']['hashtags'][0]['text'] + '\n')
             except Exception as e:
                 print (e)
 
@@ -55,5 +67,9 @@ def save_hastags(tweets):
 
 if __name__ == '__main__':
     tweets = open(sys.argv[1])
-    save_words(tweets)
+    #save_words(tweets)
+    #save_hastags(tweets)
+    #save_screen_names(tweets)
+    #save_texts(tweets)
+    save_source(tweets)
 
